9ee88397c76ecb5987fb836de3fc4b284f45116e
lmolteno <linus@molteno.net>
2023-11-08 21:41:12 +1300

    fix: convert to frame that accounts for the precession

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from astropy.coordinates import EarthLocation, get_body, AltAz
+from astropy.coordinates import EarthLocation, get_body, AltAz, PrecessedGeocentric, ITRS, solar_system_ephemeris
+from astropy.utils import iers
 import astropy.units as u
 from astropy import constants as const
 from astropy.time import Time, TimeDelta
 import numpy as np
 import cartopy.crs as ccrs
+from cartopy.feature import ShapelyFeature
+from cartopy.io.shapereader import Reader
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 # observer = EarthLocation.from_geodetic(170.5008807, -45.8792974)
@@ -12,22 +16,21 @@ def gcrs_to_earthloc(gcrs_arr, time, lon_offset):
     # gcrs = GCRS(x=gcrs_arr[0] * u.au, y=gcrs_arr[1] * u.au, z=gcrs_arr[2] * u.au, representation_type='cartesian')
     # itrs = gcrs.transform_to(ITRS(obstime=time))
     # return EarthLocation.from_geocentric(itrs.x, itrs.y, itrs.z)
-    pre_offset = EarthLocation.from_geocentric(x=gcrs_arr[0], y=gcrs_arr[1], z=gcrs_arr[2], unit=u.au).to_geodetic()
+    pre_offset = EarthLocation.from_geocentric(x=gcrs_arr[0], y=gcrs_arr[1], z=gcrs_arr[2], unit=u.km).to_geodetic()
     return EarthLocation.from_geodetic(pre_offset.lon + lon_offset, pre_offset.lat, pre_offset.height)
 
 
 def get_centerline_pos_at_time(obs_time: Time):
-    cur_moon_pos = get_body("moon", obs_time)
-    t_moon_offset = (cur_moon_pos.distance / const.c).to(u.s)
-    moon_pos = get_body("moon", obs_time + TimeDelta(t_moon_offset.value, format='sec'))
+    precessed_frame = ITRS(obstime=obs_time) # PrecessedGeocentric(equinox=obs_time, obstime=obs_time)
+    moon_pos = get_body("moon", obs_time)
+    moon_pos = moon_pos.transform_to(precessed_frame)
     moon_pos.representation_type = 'cartesian'  # geocentric xyz coords
-    moon_pos = np.array([moon_pos.x.value, moon_pos.y.value, moon_pos.z.value])
+    moon_pos = np.array([moon_pos.x.to(u.km).value, moon_pos.y.to(u.km).value, moon_pos.z.to(u.km).value])
 
-    cur_sun_pos = get_body("sun", obs_time)
-    t_sun_offset = (cur_sun_pos.distance / const.c).to(u.s)
-    sun_pos = get_body("sun", obs_time + TimeDelta(t_sun_offset.value, format='sec'))
+    sun_pos = get_body("sun", obs_time)
+    sun_pos = sun_pos.transform_to(precessed_frame)
     sun_pos.representation_type = 'cartesian'
-    sun_pos = np.array([sun_pos.x.value, sun_pos.y.value, sun_pos.z.value])
+    sun_pos = np.array([sun_pos.x.to(u.km).value, sun_pos.y.to(u.km).value, sun_pos.z.to(u.km).value])
 
     # determines direction of line through centers
     dv = (sun_pos - moon_pos) / np.linalg.norm(sun_pos - moon_pos)
@@ -42,11 +45,13 @@ def get_centerline_pos_at_time(obs_time: Time):
     centerline_location = gcrs_to_earthloc(starting_point, obs_time, lon_offset)
 
     iter_limit = 0
+    dist = np.abs(centerline_location.height.to(u.m).value)
 
-    while np.abs(centerline_location.height.to(u.m).value) > 10 and iter_limit < 100:
+    while dist > 10 and iter_limit < 100:
         iter_limit += 1
-        starting_point = starting_point - (dv * centerline_location.height.to(u.au).value)
+        starting_point = starting_point - (dv * centerline_location.height.to(u.km).value)
         centerline_location = gcrs_to_earthloc(starting_point, obs_time, lon_offset)
+        dist = np.abs(centerline_location.height.to(u.m).value)
 
     if iter_limit == 100:
         return None
@@ -55,22 +60,36 @@ def get_centerline_pos_at_time(obs_time: Time):
     return None if on_night_side else centerline_location
 
 
-start_time = Time("2028-07-22 04:10:38")
+solar_system_ephemeris.set('de432s')
+iers.conf.iers_auto_url = 'https://datacenter.iers.org/data/9/finals2000A.all'
+
+start_time = Time("2028-07-22 04:18:00")
+
 centerline_points = []
-for i in range(10):
-    obs_time = start_time + TimeDelta(i * 60.0, format='sec')
+for i in tqdm(range(20)):
+    obs_time = start_time - TimeDelta(i * 10.0, format='sec')
     pos = get_centerline_pos_at_time(obs_time)
     local_frame = AltAz(obstime=obs_time, location=pos)
     if pos is not None:
         sun_from_pos = get_body('sun', obs_time).transform_to(local_frame)
         moon_from_pos = get_body('moon', obs_time).transform_to(local_frame)
         local_dist = sun_from_pos.separation(moon_from_pos)
-        print(local_dist.to(u.arcmin).value)
         centerline_points.append(pos)
+    else:
+        print("No pos")
 
 ax = plt.axes(projection=ccrs.epsg(2193))
-ax.coastlines()
 
-lats, lons = zip(*map(lambda p: [p.lat.value, p.lon.value], centerline_points))
-plt.plot(lons, lats, transform=ccrs.Geodetic())
+x, y = zip(*map(lambda p: [p.lon.value, p.lat.value], centerline_points))
+plt.plot(x, y, transform=ccrs.Geodetic())
+# ax.coastlines()
+
+nz_coastlines = ShapelyFeature(
+    Reader("./coastlines/nz-coastlines-and-islands-polygons-topo-1250k.shp").geometries(),
+    ccrs.epsg(2193),
+    facecolors=[],
+    edgecolors=('black',)
+)
+ax.add_feature(nz_coastlines)
+
 plt.show()
